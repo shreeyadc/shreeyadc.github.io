771b13cf35f88a6d2d658241d09c26d95b8ff78a
Shreeya Champaneri <68437742+shreeyadc@users.noreply.github.com>
2025-04-23 11:48:14 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  assetPrefix: isProd ? '/your-repo-name/' : '',
+  assetPrefix: isProd ? 'shreeyadc.github.io' : '',
   images: {
     unoptimized: true,
   },
